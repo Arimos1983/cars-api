0ca6e2eef3804199ea7bee7b23dee865aaa70dcc
Arimos <amilinkovic.ns@gmail.com>
2018-08-30 16:57:34 +0200

    zadatak 3

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CarValidation;
+
 use App\Car;
 
 class CarsController extends Controller
@@ -17,17 +19,17 @@ class CarsController extends Controller
         return Car::find($id);
     }
 
-    public function store(Request $request)
+    public function store(CarValidation $request)
     {
         $car = Car::create($request->all());
         return  $car;
     }
 
-    public function update(Request $request, $id)
+    public function update(CarValidation $request, $id)
     {
-        $car = Car::findOrFail($id);
-        $car->update($request->all());
-        return $car;
+        return Car::where('id', $id)->update($request->all());
+
+
     }
 
     public function destroy($id)
