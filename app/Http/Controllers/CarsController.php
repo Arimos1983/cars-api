908cbc23b1d6fafe5143e4b0f798c0f3d026f95a
Arimos <amilinkovic.ns@gmail.com>
2018-08-30 16:14:50 +0200

    zadatak 2

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Car;
 
 class CarsController extends Controller
 {
-    //
+    public function index()
+    {
+        return Car::all();
+    }
+
+    public function show($id)
+    {
+        return Car::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $car = Car::create($request->all());
+        return  $car;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $car = Car::findOrFail($id);
+        $car->update($request->all());
+        return $car;
+    }
+
+    public function destroy($id)
+    {
+        return Car::destroy($id);
+    }
 }
